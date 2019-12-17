725031072b506e481f045eb91cdf6e1953ee11f7
YordanIH <yordan@hashicorp.com>
2019-12-17 10:04:27 +0200

    Commit

---
+++ null_resource/main.tf
@@ -0,0 +1,10 @@
+module "random_pet" {
+  source = "../random_pet/"
+
+}
+
+resource "null_resource" "hello" {
+  provisioner "local-exec" {
+    command = "echo Hello ${module.random_pet.id}"
+  }
+}
